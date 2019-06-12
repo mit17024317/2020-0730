46f59c22e0534c4d213b16d6563c67e4c6336aa7
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-06-12 16:50:33 +0900

    #6 add grouping model class

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from skfuzzy.cluster import cmeans
 import matplotlib.pyplot as plt
 from pyDOE import lhs
 import time
-
+import random
 
 class modelInterface(ABC):
     @abstractmethod
@@ -82,10 +82,36 @@ class FuzzyCM(modelInterface):
         return self.models[self.__getCloseCenter(x)]['m'].predict(np.array([x]))
 
 
+class GroupingModel(modelInterface):
+
+    def __init__(self, sampleX: list, sampleY: list):
+        lst = self.__grouping(len(sampleX[0]))
+        print(lst)
+        DIM = sampleX[0].size
+        kernel = GPy.kern.RBF(DIM)+GPy.kern.Matern32(DIM)
+        self.model = GPy.models.GPRegression(sampleX, sampleY[:, None], kernel=kernel)
+        self.model.optimize()
+
+    def __grouping(self, size):
+        lst = random.sample([x for x in range(size)], size)
+        return lst
+
+
+    def getPredictValue(self, x: list, a=2.5, b=50, c=97.5):
+        return [val[0] for val in
+                self.model.predict_quantiles(
+                    x,
+                    quantiles=(a, b, c)
+                )[1]]
+
+    def getPredict(self, x: float):
+        return self.model.predict(np.array([x]))
+
+
 if __name__ == "__main__":
     # debug parameter
     DIM = 10
-    SIZE = 1000
+    SIZE = 30 
 
     # test function
     def f(x: float):
@@ -102,23 +128,28 @@ if __name__ == "__main__":
     s1 = time.time()
     # MODEL = GPR(X, Y)
     s2 = time.time()
-    MODEL_FUZZY = FuzzyCM(X, Y)
+    # MODEL_FUZZY = FuzzyCM(X, Y)
     s3 = time.time()
+    MODEL_GROUP = GroupingModel(X, Y)
+    s4 = time.time()
     print("GPR:\t{0}ms".format(1000*(s2-s1)),
-          "FuzzyCM:\t{0}ms".format(1000*(s3-s2)), sep="\n")
+          "FuzzyCM:\t{0}ms".format(1000*(s3-s2)),
+          "Group:\t{0}ms".format(1000*(s4-s3)), sep="\n")
 
     if DIM == 1:
         # create sample point for debug
         ALL_X = np.linspace(0.0, 1.0, 500)[:, None]
         ALL_Y_TRUE = [f(x) for x in ALL_X]
         # ALL_Y_GPR = MODEL.getPredictValue(ALL_X)
-        ALL_Y_FUZZY = MODEL_FUZZY.getPredictValue(ALL_X)
+        # ALL_Y_FUZZY = MODEL_FUZZY.getPredictValue(ALL_X)
+        ALL_Y_GROUP = MODEL_GROUP.getPredictValue(ALL_X)
 
         print("-- create figure-- ")
         # create model figure
         plt.plot(ALL_X, ALL_Y_TRUE, c="r", label='function')
         # plt.plot(ALL_X, ALL_Y_GPR, c="b", label='GPR')
-        plt.plot(ALL_X, ALL_Y_FUZZY, c="y", label='Fuzzy CM')
+        # plt.plot(ALL_X, ALL_Y_FUZZY, c="y", label='Fuzzy CM')
+        plt.plot(ALL_X, ALL_Y_GROUP, c="g", label='Grouping Model')
         plt.plot(X, Y, 'o', marker=".", markersize=10, c="black", label="sample point")
         plt.legend()
 
