9e0b1c4abde25da9203849ad091f2d6b72363b5a
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-07-02 12:34:33 +0900

    #17 add experiment optimize with each EGO method

--- main.py
+++ main.py
@@ -5,25 +5,43 @@ from ego import EGO
 from model import FuzzyCM
 from model import GroupingModel
 
+from function import Schwefel
+
 if __name__ == "__main__":
     print("-- optimize start --")
+    END = 100
 
-    # test function
-    def f(x: float):
-        sum = np.sum(x)
-        return np.sin(np.sqrt(sum)*10.0)
 
-    # print("--- GroupingModel optimize ---")
-    # ego = EGO(f, 2, GroupingModel)
-    # ego.optimize(30)
-    # print("min:", np.array(ego.min).T)
-    # print("RMSE:", np.array(ego.RMSE).T)
-    # print("--- finish ---")
+    print("--- GroupingModel optimize ---")
+    ego = EGO(Schwefel, 20, [GroupingModel])
+    ego.optimize(END)
+    with open("ex/1.txt", "w") as f:
+        for x in ego.min:
+            f.write(str(x)+", ")
+        f.write("\n")
+        for x in ego.RMSE:
+            f.write(str(x)+", ")
+    print("--- finish ---")
 
-    # print("--- FuzzyCM optimize ---")
-    # ego = EGO(f, 1)
-    # ego.optimize(40)
-    # print("min:", np.array(ego.min).T)
-    # print("RMSE:", np.array(ego.RMSE).T)
-    # print("--- finish ---")
+    print("--- FuzzyCM optimize ---")
+    ego = EGO(Schwefel, 20, [FuzzyCM, GroupingModel])
+    ego.optimize(END)
+    with open("ex/2.txt", "w") as f:
+        for x in ego.min:
+            f.write(str(x)+", ")
+        f.write("\n")
+        for x in ego.RMSE:
+            f.write(str(x)+", ")
+    print("--- finish ---")
+
+    print("--- new EGO optimize ---")
+    ego = EGO(Schwefel, 20, [FuzzyCM, GroupingModel])
+    ego.optimize(END)
+    with open("ex/3.txt", "w") as f:
+        for x in ego.min:
+            f.write(str(x)+", ")
+        f.write("\n")
+        for x in ego.RMSE:
+            f.write(str(x)+", ")
+    print("--- finish ---")
 
