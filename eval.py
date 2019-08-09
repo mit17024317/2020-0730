8fceef4c6da58dc9d6397dd155ae1dd353757633
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-08-09 14:32:44 +0900

    #19 rmse sum method

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ import random
 
 
 def RMSE(dim, func, model):
-    size = 10
+    size = 10000
     if dim > RMSE.max:
         print("!! eval.py RMSE error !!")
         print("update RMSE.max greater than ", dim)
@@ -20,15 +20,13 @@ def RMSE(dim, func, model):
             np.array([[random.random() for k in range(dim)]
                       for i in range(size)])
 
-    print(RMSE.dataset[dim])
-    for t in RMSE.dataset[dim]:
-        print(t)
-        print(model.getPredict(t))
-    v = np.array([func(i) for i in RMSE.dataset[dim]]) - \
-        model.getPredictValue(RMSE.dataset[dim])
+    sum = 0.0
+    for data in RMSE.dataset[dim]:
+        sum += (func(data)-model.getPredict(data)[0])**2
 
-    v = pow(v, 2)
-    return np.sqrt(np.sum(v)/size)
+    return np.sqrt(sum/size)
+
+
 RMSE.max = 101
 RMSE.dataset = [[] for i in range(RMSE.max+1)]
 
@@ -43,20 +41,21 @@ if __name__ == "__main__":
         v = [t*np.sin(np.sqrt(abs(t))) for t in x]
         return sum(v)
 
-    sampleX = [i for i in range(3, 4)]
+    sampleX = [i for i in range(10, 20)]
     sampleY = []
     for DIM in sampleX:
+        print("dimension", DIM, sep=": ")
 
         # create sample point
         X = np.array([[s[i] for i in range(DIM)] for s in lhs(DIM, SIZE)])
-        Y = np.array([f(i) for i in X]) 
+        Y = np.array([f(i) for i in X])
         model = FuzzyCM(X, Y)
-        
+
         # calc RMSE
         val = RMSE(DIM, f, model)
         sampleY.append(val)
         print("dim= ", DIM, ": ", val)
-    
+
     plt.plot(sampleX, sampleY, marker=".", markersize=10, c="black")
     plt.legend()
 
--- test_eval.py
+++ test_eval.py
@@ -4,9 +4,8 @@ from testModel import testFunc, testModel
 
 
 @pytest.mark.parametrize(("dim"), [
-#    (1), (2), (10), (50), (100)
-    (3)
+    (1), (2), (10), (50), (100)
 ])
 def test_RMSE(dim):
-    rmse = RMSE(dim, testFunc, testModel)
-    assert rmse == 5*10000
+    rmse = RMSE(dim, testFunc, testModel())
+    assert rmse == 5
