746148f553f2ed087d342e74086caa7fe4dddc6e
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2020-02-12 04:35:20 +0900

    #23 add EI test

--- Optimizer/Acquisition/test/test_EHVI.py
+++ Optimizer/Acquisition/test/test_EHVI.py
@@ -1,13 +1,59 @@
 import os
 import sys
 
+import numpy as np
 import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
-module = __import__("Acquisition", fromlist=["EI, EHVI"])
+module = __import__("Acquisition", fromlist=["EI", "EHVI"])
 
 
 class TestEHVI:
-    @pytest.mark.skip
-    def test_f(self, mean, var, pops) -> float:
-        ...
+    @pytest.mark.parametrize(
+        ("mean", "var", "pops"),
+        [
+            (np.array([0]), np.array([1]), [np.array([0.5])]),
+            (np.array([0]), np.array([1]), [np.array([0.8]), np.array([0.1])]),
+            (np.array([0.3]), np.array([0.2]), [np.array([0.2])]),
+            (np.array([0.5]), np.array([6.1]), [
+             np.array([0.6]), np.array([0.7])]),
+        ],
+    )
+    def test_f_1dim(self, mean, var, pops) -> float:
+        AF = module.EI.EI()
+        ei = AF.f(mean[0], var[0], np.min(pops))
+        AFM = module.EHVI.EHVI()
+        ehvi = AFM.f(mean, var, pops)
+        dif = np.abs(ei - ehvi)
+        assert dif < 1e-5
+
+    @pytest.mark.parametrize(
+        ("mean", "var", "pops"),
+        [
+            (np.array([0]), np.array([1]), [np.array([0.5])]),
+            (
+                np.array([0, 0]),
+                np.array([1, 1]),
+                [np.array(l) for l in [[0.3, 0.2], [0.1, 0.8]]],
+            ),
+            (
+                np.array([0.2, 0, 0.3]),
+                np.array([10, 0.1, 0.8]),
+                [
+                    np.array(l)
+                    for l in [
+                        [0.11, 0.3, 0.2],
+                        [0.3123, 0.1, 0.8],
+                        [0.3123, 0.1, 0.8],
+                        [0.3123, 0.1, 0.8],
+                        [0.3123, 0.1, 0.8],
+                        [0.3123, 0.1, 0.8],
+                    ]
+                ],
+            ),
+        ],
+    )
+    def test_f_nonError(self, mean, var, pops) -> float:
+        AFM = module.EHVI.EHVI()
+        ehvi = AFM.f(mean, var, pops)
+        print(ehvi)
--- Optimizer/Acquisition/test/test_EI.py
+++ Optimizer/Acquisition/test/test_EI.py
@@ -1,13 +1,32 @@
 import os
 import sys
 
+import numpy as np
 import pytest
+from numpy.random import normal
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 module = __import__("Acquisition", fromlist=["EI"])
 
 
 class TestEI:
-    @pytest.mark.skip
-    def test_f(self, mean, var, basis) -> float:
-        ...
+    @pytest.mark.parametrize(
+        ("mean", "var", "base"),
+        [(0.0, 1.0, 0.5), (0.8, 0.1, 0.3), (0.1, 1.3, 0.7), (0.4, 0.1, 0.3), ],
+    )
+    def test_f(self, mean, var, base) -> float:
+        # 厳密解
+        AF = module.EI.EI()
+        ei = AF.f(mean, var, base)
+
+        # 近似解(モンテカルロ積分)
+        sum = 0.0
+        size = 100000
+        for x in normal(mean, var, size):
+            sum += np.max([base - x, 0.0])
+        sum /= size
+        print(sum, ei)
+
+        # 差分
+        dif = np.abs(sum - ei)
+        assert dif < 0.1
