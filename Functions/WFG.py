d2a5ee1f659ae851e7e41baf25e42e75db5996cd
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2020-02-24 14:36:12 +0900

    #30 add WFG on optproblems

---
+++ Functions/WFG.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""WFG Test Problem."""
+
+__author__ = "R.Nakata"
+__date__ = "2020/02/24"
+
+
+import numpy as np
+from optproblems import Individual, wfg
+
+
+def evaluateWFG(WFG: wfg.WFG, x: np.ndarray) -> np.ndarray:
+    indiv: Individual = Individual(x)
+    WFG.evaluate(indiv)
+    val = np.array(indiv.objective_values)
+    return val
+
+
+class WFG1:
+    def f(self, x: np.ndarray) -> np.ndarray:
+        prob: wfg.WFG1 = wfg.WFG1(2, len(x), 4)
+        val: np.ndarray = evaluateWFG(prob, x)
+        return val
